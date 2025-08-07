8682841409f45f64b2c88e2fa5a97c2adef99a2a
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-07 13:23:53 +0530

    updated

--- 02CoreModule/02osModule.js
+++ 02CoreModule/02osModule.js
@@ -1,15 +1,15 @@
 
 
-console.log("platform", os.platform()); // Outputs the OS platform
-console.log("release",os.release()); // Outputs the OS release version    
-console.log("totalmem",os.totalmem()); // Outputs the total memory in bytes
-console.log("freemem",os.freemem()); // Outputs the free memory in bytes
-console.log("cpus",os.cpus()); // Outputs information about the CPU cores
-console.log("homedir",os.homedir()); // Outputs the home directory of the current user
-console.log("uptime",os.uptime()); // Outputs the system uptime in seconds
-console.log("networkInterfaces",os.networkInterfaces()); // Outputs network interfaces information
-console.log("tmpdir",os.tmpdir()); // Outputs the temporary directory path
-console.log("arch",os.arch()); // Outputs the architecture of the operating system
-console.log("hostname",os.hostname()); // Outputs the hostname of the operating system
-console.log("endianness",os.endianness()); // Outputs the endianness of the CPU
-console.log("endianness",os.constants()); // Outputs the OS constants
+console.log("platform", OS.platform()); // Outputs the OS platform
+console.log("release",OS.release()); // Outputs the OS release version    
+console.log("totalmem",OS.totalmem()); // Outputs the total memory in bytes
+console.log("freemem",OS.freemem()); // Outputs the free memory in bytes
+console.log("cpus",OS.cpus()); // Outputs information about the CPU cores
+console.log("homedir",OS.homedir()); // Outputs the home directory of the current user
+console.log("uptime",OS.uptime()); // Outputs the system uptime in seconds
+console.log("networkInterfaces",OS.networkInterfaces()); // Outputs network interfaces information
+console.log("tmpdir",OS.tmpdir()); // Outputs the temporary directory path
+console.log("arch",OS.arch()); // Outputs the architecture of the operating system
+console.log("hostname",OS.hostname()); // Outputs the hostname of the operating system
+console.log("endianness",OS.endianness()); // Outputs the endianness of the CPU
+console.log("endianness",OS.constants()); // Outputs the OS constants
