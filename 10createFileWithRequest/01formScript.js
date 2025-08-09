a334bef0d2bbd917af9c5808f858ddb86c18ab6e
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-09 18:56:05 +0530

    updated

--- 10createFileWithRequest/01formScript.js
+++ 10createFileWithRequest/01formScript.js
@@ -7,9 +7,9 @@ http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         fs.readFile('01Form.html', (err, data) => {
             if (err) {
-                res.writeHead(404, {'Content-Type':'text/plain'});
+                res.writeHead(404, {'Content-Type':'text/html'});
                 res.write('<h1>404 Not Found</h1>');
-            }else {
+            }else if (req.url === '/') {
                 res.writeHead(200, {'Content-Type':'text/html'});
                 res.write(data);
             }
@@ -25,18 +25,33 @@ http.createServer((req, res) => {
         // folder check
         if(!fs.existsSync('file')){
             fs.mkdirSync('file');
-        }  
+        }
 
-        // Responding to the client
+       // Responding to the client
         req.on('end', () => {
             let bodyData = Buffer.concat(body).toString();
             bodyData = querystring.parse(bodyData);
-            
             console.log('Form Data Received:', bodyData);
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write('<h1>Form Submitted</h1>');
-            res.end();
+           
+
+            fs.writeFile(`file/${bodyData.username}.txt`, `username = ${bodyData.username} & password = ${bodyData.password}`, 'utf-8', (err) => {
+                if (err) {
+                    console.error('Error writing file:', err);
+                    res.writeHead(500, {'Content-Type':'text/html'});
+                    res.write('<h1>Internal Server Error</h1>');
+                    return res.end();
+                }else {
+                    console.log('File created successfully');
+                }
+
+            });
+
+
         });
+
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('<h1>Form Submitted</h1>');
+        res.end();
     }
 
 }).listen(3535)
