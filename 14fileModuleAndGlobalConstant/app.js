1f1209b6d74232680577a38a602a26cc7c26a593
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-13 18:57:59 +0530

    updated

--- 14fileModuleAndGlobalConstant/app.js
+++ 14fileModuleAndGlobalConstant/app.js
@@ -1,8 +1,15 @@
 
 const path = require('path')
 const file = "files/sujit.txt"
- 
+
+// Path Module
 console.log(path.dirname(file))  // files
 console.log(path.extname(file))  // .txt
 console.log(path.basename(file))  // sujit.txt
 console.log(path.resolve("files", 'sujit.txt')) // d:\SKS\learning\Node\files\sujit.txt
+console.log(path.isAbsolute(file)) // false
+
+
+// Global Constent
+console.log(__dirname) // d:\SKS\learning\Node\14fileModuleAndGlobalConstant
+console.log(__filename) // d:\SKS\learning\Node\14fileModuleAndGlobalConstant\app.js
